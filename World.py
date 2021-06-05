1d2fa2d06b9102860bcebdbfff7a37562b133dc3
Mikafur32 <person.minecraft@hotmail.com>
2021-06-05 16:04:45 -0400

    Created a boolean 2d array verifying whether or not there exists an ant in the cell. Added __str__ to string for World.py. Updated TODO lists. Updated Docstrings.

--- World.py
+++ World.py
@@ -2,12 +2,6 @@ from Ant import Ant
 
 
 class World:
-    '''
-
-    '''
-    # TODO:
-    #  DONE 1. Create list of ants
-    #  2. Use list of ants to create a nextGeneration function
 
 
     def __init__(self, y_columns, x_rows, numAnts):
@@ -19,20 +13,56 @@ class World:
             The width of the grid (world) the ants will be contained in.
         y_columns : int
             The height of the grid.
+        worldGrid : 2d List(Boolean)
+            Boolean array of the locations of the ants.
+            Parameters : x_rows, y_columns
         numAnts : int
             The number of ants in the simulation.
-
+        antList : List(Ant)
+            A list containing all of the ants in the simulation.
+            Parameters : numAnts
         '''
         self.y_columns = y_columns
         self.x_rows = x_rows
+        self.worldGrid = [x_rows][y_columns]
         self.numAnts = numAnts
-        antList = Ant([0, 0])[numAnts]
+        self.antList = Ant([x_rows / 2,y_columns / 2 ])[numAnts] # middle of the grid
 
     def nextGeneration(self):
-        '''
-        Updates each ant in antList, calling nextPosition on each
+        '''Updates each ant in antList, calling nextPosition on each ant in the list.'''
+        #TODO
+        # What happens if more than one ant is in the cell?
+        for ants in self.antList:
+            self.worldGrid[ants.row][ants.column] = False
+            ants.nextPosition()
+            self.worldGrid[ants.row][ants.column] = True
 
-        Returns
-        -------
-        NONE
+    def containsAnt(self,row,column):
+        '''Verifies the presence of an ant given a row and column.'''
+        return self.worldGrid[row][column]
+
+    def __str__(self):
+        '''A Conway's Game of Life inspired row x column grid containing "live" cells/ants and "non-living" cells.        '''
+        for row in self.x_rows:
+            for column in self.y_columns:
+                if self.containsAnt(row,column):
+                    print("\u2588" + "\u2588", end="")
+                else:
+                    print("  ", end="")
+            print("\n")
+
+        '''
+        private static void printGrid(World world)
+        {
+            for (int row = 0; row < world.getHeight(); row++)
+            {
+                for (int column = 0; column < world.getWidth(); column++)
+                {
+                    System.out.print((world.getCell(row, column))
+                        ? ("\u2588" + "\u2588")
+                        : "  ");
+                }
+                System.out.println();
+            }
+        }
         '''
